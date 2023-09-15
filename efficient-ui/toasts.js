1636a3cd4d94ba6c59b94fbcfb504d410a35b2eb
vlad <vlad.muntoiu@gmail.com>
2023-09-15 07:16:22 +0300

    Toasts

---
+++ efficient-ui/toasts.js
@@ -0,0 +1,19 @@
+function removeToast() {
+    const button = event.target;
+    el = button;
+
+    // Check for the parent toast, remove it
+    while (el && el.parentNode) {
+        el = el.parentNode;
+        
+        if (el && el.parentNode.classList && el.parentNode.classList.contains("toast-container")) {
+            el.classList.add("removing")
+            el.addEventListener("animationend", () => {
+                el.remove();
+            });
+            break;
+        }
+    }
+        
+    buttonGrandparent.remove();
+}  
